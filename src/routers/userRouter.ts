2adcb4beaac4622d90507c2d5c8f2bf8e22e2707
Iagohss <iago.silva@ccc.ufcg.edu.br>
2025-02-04 22:09:47 -0300

    docs: criada a documentação do projeto com Swagger

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -3,15 +3,119 @@ import UserController from '../controllers/userController';
 
 export const router = express();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: Endpoints para gerenciamento de usuários
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Cria um novo usuário
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               admin:
+ *                 type: boolean
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso
+ *       400:
+ *         description: Erro na criação do usuário
+ */
 router.post('/', (req, res, next) => UserController.createUser(req, res, next));
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Busca um usuário pelo ID
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID do usuário
+ *     responses:
+ *       200:
+ *         description: Usuário encontrado
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.get('/:id', (req, res, next) => {
   UserController.getUser(req, res, next)
   return;
 });
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Atualiza um usuário existente
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               admin:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.put('/:id', (req, res, next) => UserController.updateUser(req, res, next));
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Deleta um usuário
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Usuário deletado com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ */
 router.delete('/:id', (req, res, next) => UserController.deleteUser(req, res, next));
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import app from "./app";
+import { setupSwagger } from "./swagger";
 
 const path = require('path');
 
@@ -10,9 +11,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+setupSwagger(app);
+
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
